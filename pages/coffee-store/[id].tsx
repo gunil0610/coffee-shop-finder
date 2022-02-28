71412bce6207e78407936194fcb7c6521200b308
gunil0610 <gunil.na1@gmail.com>
2022-02-28 17:37:03 +0900

    feat: implement getStaticProps and getStaticPaths

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -2,7 +2,29 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const CoffeeStore: NextPage = () => {
+import { CafeList } from "../../common/types/cafeList";
+import cafeData from "../../data/coffee-stores.json";
+
+export function getStaticProps({ params }: any) {
+  return {
+    props: {
+      cafe: cafeData.find((cafe) => cafe.id.toString() === params.id),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+}
+
+interface Props {
+  cafe: CafeList;
+}
+
+const CoffeeStore: NextPage<Props> = ({ cafe }) => {
   const router = useRouter();
 
   return (
@@ -11,6 +33,8 @@ const CoffeeStore: NextPage = () => {
       <Link href="/">
         <a>Back to home</a>
       </Link>
+      <p>{cafe.address}</p>
+      <p>{cafe.name}</p>
     </div>
   );
 };
